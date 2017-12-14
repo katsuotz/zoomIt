07e9156be141fe6d58e209dfc81e84f1b1dc23f1
Irfan Fakhri <m.irfan.fakhri66@gmail.com>
2017-12-14 17:50:07 +0700

    Initial Commit

---
+++ zoomIt.js
@@ -0,0 +1,106 @@
+(function ($) {
+    $.fn.zoomIt = function (data) {
+
+    	var mainImage = $(this);
+
+    	var slider = data.slider;
+    	if (slider) {
+    		var currentMargin = 0;
+    		var currentPosition = 0;
+    		var imageList = slider.imageList;
+    		var imageListWidth = [];
+    		var imageListWidthTotal = 0;
+
+    		if (slider.width) {
+    			$('.image-overlay').width(slider.width);
+    		}
+
+    		$(document).on('click', imageList + ' img', function () {
+    			mainImage.attr('src', $(this).attr('src'));
+    		});
+
+    		if ($(imageList + ' img').length) {
+    			$.each($(imageList + ' img'), function() {
+        			var width = $(this).width();
+        			imageListWidth.push(width);
+        			imageListWidthTotal += width;
+        		});
+    		}
+
+    		$(imageList).css('width', imageListWidthTotal);
+
+    		$('.arrow.prev-image').click(function () {
+    			if (currentPosition > 0) {
+        			currentMargin += imageListWidth[currentPosition - 1];
+        			currentPosition -= 1;
+        			console.log(currentMargin);
+        			$(imageList).css('margin-left', currentMargin + 'px');
+	        		console.log(currentPosition);
+    			}
+    		});
+
+    		$('.arrow.next-image').click(function () {
+    			if (currentPosition < imageListWidth.length - 1) {
+        			currentMargin -= imageListWidth[currentPosition];
+        			currentPosition += 1;
+        			console.log(currentMargin);
+        			$(imageList).css('margin-left', currentMargin + 'px');
+	        		console.log(currentPosition);
+    			}
+    		});
+
+    	}
+
+    	$(this).css('cursor', 'zoom-in');
+    	$(this).click(function () {
+
+    		var src = $(this).attr('src');
+
+    		$('.zoomed-image').attr('src', src);
+
+        	if (slider) {
+        		if ($(imageList + ' img').length) {
+        			$('.zoom-image-content').empty();
+        			$.each($(imageList + ' img'), function() {
+        				$('.zoom-image-content').append('<img src="' + $(this).attr('src') +'">')
+        			});
+        		}
+            }
+
+        	$('.zoomed-image').css({
+        		'cursor': 'zoom-out'
+        	});
+
+        	$('.zoom-overlay').css({
+        		'opacity': '1',
+        		'visibility': 'visible',
+        	});
+    	});
+
+		$(document).on('click', '.zoomed-image', function () {
+        	$('.zoomed-image').css({
+        		'cursor': 'default'
+        	});
+
+        	$('.zoom-overlay').css({
+        		'opacity': '0',
+        	});
+
+        	setTimeout( function() {
+	        	$('.zoom-overlay').css({
+	        		'visibility': 'hidden',
+	        	});
+        	}, 400);
+		});
+
+		$(document).on('click', '.zoom-image-content img', function () {
+			var src = $(this).attr('src');
+
+        	$('.zoomed-image').attr('src', src);
+
+		});
+
+
+        return this;
+    };
+})( jQuery );
