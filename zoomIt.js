fc421d6918996294e976ffa1cddf8a79518658f9
Irfan Fakhri <m.irfan.fakhri66@gmail.com>
2017-12-15 10:07:27 +0700

    fix

--- zoomIt.js
+++ zoomIt.js
@@ -16,17 +16,56 @@
 
     	var slider = data.slider;
     	if (slider) {
-    		var currentMargin = 0;
-    		var currentPosition = 0;
-    		var imageList = slider.imageList;
-    		var imageListWidth = [];
-    		var imageListWidthTotal = 0;
+            var currentMargin = 0;
+            var currentPosition = 0;
+            var imageList = slider.imageList;
+            var imageListWidth = [];
+            var imageListWidthTotal = 0;
+            var imageListHeight = 80;
+            var imageMargin = {
+                l: 5,
+                r: 5,
+                t: 0,
+                b: 0
+            }
 
-    		$(imageList).wrap('<div class="image-overlay"><div class="image-slider"></div></div>');
+            $(imageList).css('transition', '.3s');
+            $(imageList + ' div').css({
+                'float': 'left',
+                'cursor': 'pointer'
+            });
 
-    		if (slider.width) {
-    			$('.image-overlay').width(slider.width);
-    		}
+            $(imageList).wrap('<div class="image-overlay"><div class="image-slider"></div></div>');
+
+            if (slider.width) {
+                $('.image-overlay').width(slider.width);
+            }
+
+            if (slider.list.height) {
+                imageListHeight = slider.list.height;
+            }
+
+            $(imageList + ' img').height(imageListHeight);
+
+            if (slider.list.margin) {
+                if (slider.list.margin.l) {
+                    imageMargin.l = slider.list.margin.l;
+                }
+                if (slider.list.margin.r) {
+                    imageMargin.r = slider.list.margin.r;
+                }
+                if (slider.list.margin.t) {
+                    imageMargin.t = slider.list.margin.t;
+                }
+                if (slider.list.margin.b) {
+                    imageMargin.b = slider.list.margin.b;
+                }
+
+                $(imageList + ' img').css('margin-left', imageMargin.l + 'px');
+                $(imageList + ' img').css('margin-right', imageMargin.r + 'px');
+                $(imageList + ' img').css('margin-top', imageMargin.t + 'px');
+                $(imageList + ' img').css('margin-bottom', imageMargin.b + 'px');
+            }
 
     		$(document).on('click', imageList + ' img', function () {
                 var src = $(this).attr('src');
@@ -36,35 +75,42 @@
                 });
     		});
 
-    		if ($(imageList + ' img').length) {
-    			$.each($(imageList + ' img'), function() {
+    		if ($(imageList + ' div').length) {
+    			$.each($(imageList + ' div'), function() {
         			var width = $(this).width();
-        			imageListWidth.push(width);
-        			imageListWidthTotal += width;
-        		});
-    		}
+                    var margin = {
+                        x: parseInt($(this).css('margin-left')) + parseInt($(this).css('margin-right')),
+                        y: parseInt($(this).css('margin-top')) + parseInt($(this).css('margin-bottom')),
+                    }
 
-    		$(imageList).css('width', imageListWidthTotal);
+        			imageListWidth.push(width + (margin.x));
+        			imageListWidthTotal += width + (margin.x);
+                });
+            }
 
-    		$(document).on('click', '.arrow.prev-image', function () {
-    			if (currentPosition > 0) {
-        			currentMargin += imageListWidth[currentPosition - 1];
-        			currentPosition -= 1;
-        			$(imageList).css('margin-left', currentMargin + 'px');
-    			}
-    		});
-
-    		$(document).on('click', '.arrow.next-image', function () {
-    			if (currentPosition < imageListWidth.length - 1) {
-        			currentMargin -= imageListWidth[currentPosition];
-        			currentPosition += 1;
-        			$(imageList).css('margin-left', currentMargin + 'px');
-    			}
-    		});
+            $(imageList).width(imageListWidthTotal + 1);
 
     		$('.image-overlay').prepend(
     			'<span class="arrow prev-image"><</span>\
     			<span class="arrow next-image">></span>');
+            
+            $(document).on('click', '.arrow.prev-image', function () {
+                if (currentPosition > 0) {
+                    currentMargin += imageListWidth[currentPosition - 1];
+                    currentPosition -= 1;
+                    $(imageList).css('margin-left', currentMargin + 'px');
+                }
+            });
+
+            $(document).on('click', '.arrow.next-image', function () {
+                if (currentPosition < imageListWidth.length - 1) {
+                    currentMargin -= imageListWidth[currentPosition];
+                    currentPosition += 1;
+                    console.log(imageList);
+                    $(imageList).css('margin-left', currentMargin + 'px');
+                }
+            });
+
 
     	}
 
@@ -78,12 +124,20 @@
     		$('.zoomed-image').attr('src', src);
 
         	if (slider) {
-        		if ($(imageList + ' img').length) {
+        		if ($(imageList + ' div img').length) {
         			$('.zoom-image-content').empty();
-        			$.each($(imageList + ' img'), function() {
+        			$.each($(imageList + ' div img'), function() {
         				$('.zoom-image-content').append('<img src="' + $(this).attr('src') +'">')
         			});
         		}
+
+                var sliderZoomHeight = 50;
+
+                if (slider.zoomList.height) {
+                    sliderZoomHeight = slider.zoomList.height;
+                    $('.zoom-image-content img').height(sliderZoomHeight);
+                }
+
             }
             setTimeout( function() {
                 $('.zoom-overlay').addClass('zoom-active');
@@ -105,9 +159,7 @@
 
             $('body').css('overflow-y', 'scroll');
 
-            setTimeout( function() {
-                $('.zoom-overlay').removeClass('zoom-active');
-            }, 0);		
+            $('.zoom-overlay').removeClass('zoom-active');
 
         	setTimeout( function() {
 	        	$('.zoom-overlay').css({
@@ -118,7 +170,7 @@
 
 		$(document).on('click', '.zoom-image-content img', function () {
 			var src = $(this).attr('src');
-            
+
             $('.zoomed-image').fadeOut('fast', function () {
                 $('.zoomed-image').attr('src', src);
                 $('.zoomed-image').fadeIn('fast');
